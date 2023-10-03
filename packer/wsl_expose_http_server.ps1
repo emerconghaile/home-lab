346599d71c2a2eef2e9731fbae610efcd53a09f8
Emer Connelly <emerconn7@gmail.com>
2023-10-03 00:29:19 -0500

    .

--- packer/wsl_expose_http_server.ps1
+++ packer/wsl_expose_http_server.ps1
@@ -1,4 +1,8 @@
 $port = <port>
-$wsl_ip = <wsl-ip>
-netsh interface portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$wsl_ip
+$wsl_ip = "<wsl-ip>"
+netsh int portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$wsl_ip
 netsh advfirewall firewall add rule name="Open Port ${$port}" dir=in action=allow protocol=TCP localport=$port
+netsh int portproxy show v4tov4
+netsh advfirewall firewall show rule name="Open Port ${$port}"
+# netsh int portproxy reset
+# netsh advfirewall firewall delete rule name="Open Port ${$port}"
