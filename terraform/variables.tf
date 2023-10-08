8125c3a11fb04c474ea5220c2a18c054ecd26494
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-10-08 14:26:44 -0500

    unused variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,6 @@ variable "home_lab_password" {
   type        = string
   sensitive   = true
 }
-variable "ssh_public_key" {
-  description = "Global home lab SSH public key"
-  type        = string
-}
 # Proxmox provider
 variable "pm_api_url" {
   description = "Proxmox API URL"
